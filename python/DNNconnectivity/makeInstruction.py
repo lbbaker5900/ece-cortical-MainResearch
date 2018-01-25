55f90f484ba4a8edb779ab51be5d910aff3b544c
lbbaker <lbbaker@ncsu.edu>
2018-01-25 18:18:59 -0500

    TEX: rearrange layout - WIP

--- python/DNNconnectivity/makeInstruction.py
+++ python/DNNconnectivity/makeInstruction.py
@@ -6,28 +6,36 @@ a = '00000000_00000000__00000000_00000000__00000000_00000000____00100___11_____1
 #                       7      0  15    8   24     16                                                     
 #                                                                                                    
 #                                                                                                    
-#                       gggggg                                                                         
-#                       PPPPPP   h                                                                     
-#                       tttttt a o            iii                                                       
-# Mode reg              rrrrrr l s         e  ddd nnnnn       sync=10     CFG                                                        
-#                       543210 l t         n  210 aaaaa                                                   
-a = '00000000_00000000__000000_0_1__00000000__000_00000______00001010____00100___11_____11'
+a = []
+# Mode reg = sync send 
+#                            gggggg                                                                         
+#                            PPPPPP   h                                                                     
+#                            tttttt a o            iii                                                       
+#                            rrrrrr l s         e  ddd nnnnn       sync=10     CFG                                                        
+#                            543210 l t         n  210 aaaaa                                                   
+a.append('00000000_00000000__000000_0_1__00000000__000_00000______00001010____00100___11_____11')
+print  '// Sync send'
+for i in a:
+  print  hex(int(i.replace('_',''),2)).split('x')[1]
+
 
 
 a = []
 
 # Mode reg = upld 
+#   upld:id=011
 #                            cccccccc  cccccccc      ccccc                                                
 #                            nnnnnnnn  nnnnnnnn      nnnnn                                                
 #                            tttttttt  tttttttt  iii ttttt                                                 
-#                            00000000  11111100  ddd 21111       upld=11     CFG                                                        
+#                            00000000  11111100  ddd 21111       data=11     CFG                                                        
 #                            76543210  54321098  210 09876                                                   
-a.append('00000000_00000000__00000000__00000000__011_01000______00001011____00100___01_____01')
+a.append('00000000_00000000__01000001__00000000__011_00000______00001011____00100___01_____01')
 
 #           val2     opt2     val1     opt1      val0     opt0       OP    dcntl  icntl
 a.append('00000010_00000100__00000000_00000011__00000011_00000010____00100___00_____00')
 a.append('00000000_00000000__00000000_00000000__00000000_00000111____00100___10_____10')
 
+print  '// Upload'
 for i in a:
   print  hex(int(i.replace('_',''),2)).split('x')[1]
 
